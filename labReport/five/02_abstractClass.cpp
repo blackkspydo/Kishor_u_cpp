51629aa582ab4c8500fcab1fd0af65a3c444a631
Kishor U <upadhyaya13kishor@gmail.com>
2023-03-29 20:10:14 +0545

    abstractCls

---
+++ labReport/five/02_abstractClass.cpp
@@ -0,0 +1,31 @@
+// 2. Write a program to show the use of abstract class and pure virtual function.
+
+#include <iostream>
+using namespace std;
+
+class Shape
+{
+public:
+    virtual double area() = 0;
+};
+
+class Circle : public Shape
+{
+private:
+    double radius;
+
+public:
+    Circle(double r) : radius(r) {}
+    double area() override
+    {
+        return 3.14159 * radius * radius;
+    }
+};
+
+int main()
+{
+    Circle circle(5);
+    cout << "Area of circle: " << circle.area() << endl;
+
+    return 0;
+}
