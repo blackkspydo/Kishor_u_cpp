1b194a1a8f4f072bcc57512425fbe461fc5f3141
blackkspydo <upadhyaya13kishor@gmail.com>
2023-04-04 19:01:49 +0545

    floor and ceil

---
+++ discreteStructure/03_floor&ceil.c
@@ -0,0 +1,16 @@
+// Program in C to find ceiling and floor function.
+
+#include <stdio.h>
+
+int main()
+{
+    double num = 4.7;
+    int ceil_x, floor_x;
+    // using typecasting ti truncate the decimal part and add 1 to get the ceiling
+    ceil_x = (int)num + ((num > (int)num) ? 1 : 0);
+    // using typecasting ti truncate the decimal part to get the floor
+    floor_x = (int)num - ((num < (int)num) ? 1 : 0);
+    printf("Ceiling of %.2f is %d\n", num, ceil_x);
+    printf("Floor of %.2f is %d\n", num, floor_x);
+    return 0;
+}
